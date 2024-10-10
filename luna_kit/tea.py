43bf0845b16b8c153543d6e6027767489fb920d7
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-10-10 10:05:41 -0600

    add `encrypt()` function
    Now I can write new .ark files...

--- luna_kit/tea.py
+++ luna_kit/tea.py
@@ -1,46 +1,9 @@
-from ctypes import c_uint32
-import sys
 from ctypes import *
+from ctypes import c_uint32
 from typing import Annotated
 
-def encipher(v, k):
-    y = c_uint32(v[0])
-    z = c_uint32(v[1])
-    sum = c_uint32(0)
-    delta = 0x9e3779b9
-    n = 32
-    w = [0,0]
 
-    while(n>0):
-        sum.value += delta
-        y.value += ( z.value << 4 ) + k[0] ^ z.value + sum.value ^ ( z.value >> 5 ) + k[1]
-        z.value += ( y.value << 4 ) + k[2] ^ y.value + sum.value ^ ( y.value >> 5 ) + k[3]
-        n -= 1
-
-    w[0] = y.value
-    w[1] = z.value
-    return w
-
-def decipher(v, k):
-    y = c_uint32(v[0])
-    z = c_uint32(v[1])
-    sum = c_uint32(0xc6ef3720)
-    delta = 0x9e3779b9
-    n = 32
-    w = [0,0]
-
-    while(n>0):
-        z.value -= ( y.value << 4 ) + k[2] ^ y.value + sum.value ^ ( y.value >> 5 ) + k[3]
-        y.value -= ( z.value << 4 ) + k[0] ^ z.value + sum.value ^ ( z.value >> 5 ) + k[1]
-        sum.value -= delta
-        n -= 1
-
-    w[0] = y.value
-    w[1] = z.value
-    return w
-
-
-def get_xxtea_phdr_size(phdr_off: int):
+def get_phdr_size(phdr_off: int):
     if (phdr_off & 3):
         phdr_off &= ~3
         phdr_off += 4
@@ -48,7 +11,7 @@ def get_xxtea_phdr_size(phdr_off: int):
     return phdr_off
 
 
-def xxtea_decrypt(src: bytes | bytearray, n: int, key: Annotated[list[int], 4]):
+def decrypt(src: bytes | bytearray, n: int, key: Annotated[list[int], 4]):
     v = [
         c_uint32(int.from_bytes(src[x * len(src) // n : (x + 1)* len(src) // n], 'little'))
         for x in range(n)
@@ -84,6 +47,43 @@ def xxtea_decrypt(src: bytes | bytearray, n: int, key: Annotated[list[int], 4]):
 
         rounds -= 1
 
+    return b''.join([bytes(x) for x in v])
 
+def encrypt(src: bytes | bytearray, n: int, key: Annotated[list[int], 4]):
+    v = [
+        c_uint32(int.from_bytes(src[x * len(src) // n : (x + 1)* len(src) // n], 'little'))
+        for x in range(n)
+    ]
+
+    key: list[c_uint32] = [c_uint32(k) for k in key]
+
+    DELTA = 0x9e3779b9
+
+    y = c_uint32(0)
+    z = c_uint32(0)
+    p = 0
+    e = c_uint32(0)
+
+    def MX():
+        return (((z.value >> 5 ^ y.value << 2) + (y.value >> 3 ^ z.value << 4)) ^ ((sum.value ^ y.value) + (key[(p & 3) ^ e.value].value ^ z.value)))
+
+    rounds = 6 + (52 // n)
+    sum = c_uint32(0)
+    z.value = v[n - 1].value
+
+    while (rounds):
+        sum.value += DELTA
+        
+        e.value = (sum.value >> 2) & 3
+        for p in range(0, n - 1, 1):
+            y.value = v[p + 1].value
+            v[p].value += MX()
+            z.value = v[p].value
+        p = n - 1
+        y.value = v[0].value
+        v[n - 1].value += MX()
+        z.value = v[n - 1].value
+
+        rounds -= 1
 
     return b''.join([bytes(x) for x in v])
