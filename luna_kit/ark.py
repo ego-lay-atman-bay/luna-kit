8bfe8d428691fef1191ad57c06e7a683c8a7f226
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-10-10 21:32:35 -0600

    add writing .ark files

--- luna_kit/ark.py
+++ luna_kit/ark.py
@@ -2,28 +2,46 @@ import contextlib
 import hashlib
 import io
 import os
+import struct
 import warnings
+from collections import namedtuple
 from ctypes import *
-from typing import IO, BinaryIO, Literal
+from dataclasses import dataclass
+from typing import IO, BinaryIO, Literal, NamedTuple, Annotated
 
-from rich.progress import Progress, track
+import dataclasses_struct as dcs
 import zstandard
+from rich.progress import Progress, track
 
-from . import enums, tea, types
+from . import enums, types, xxtea
 from .console import console
 from .utils import is_binary_file, is_text_file, posix_path, trailing_slash
 
-FILE_METADATA_MAP = {
-    "filename": 128,
-    "pathname": 128,
-    "file_location": 4,
-    "original_filesize": 4,
-    "compressed_size": 4,
-    "encrypted_nbytes": 4,
-    "timestamp": 4,
-    "md5sum": 16,
-    "priority": 4,
-}
+
+@dcs.dataclass()
+class Header():
+    file_count: dcs.U32 = 0
+    metadata_offset: dcs.U32 = 0
+    ark_version: dcs.U32 = 0
+
+HEADER_FORMAT = "3I"
+
+@dcs.dataclass()
+class FileMetadata():
+    filename: Annotated[bytes, 128]
+    pathname: Annotated[bytes, 128]
+    file_location: dcs.U32
+    original_filesize: dcs.U32
+    compressed_size: dcs.U32
+    encrypted_nbytes: dcs.U32
+    timestamp: dcs.U32
+    md5sum: Annotated[bytes, 16]
+    priority: dcs.U32
+
+FILE_METADATA_FORMAT = "128s128s5I16sI"
+
+def decode(b: bytes, encoding: str = 'ascii'):
+    return b.decode(encoding).rstrip('\x00')
 
 def read_bytes(b: bytes | bytearray, order: Literal[enums.ENDIAN.BIG, enums.ENDIAN.LITTLE] = enums.ENDIAN.BIG):
     if order == enums.ENDIAN.LITTLE:
@@ -35,14 +53,8 @@ def read_bytes(b: bytes | bytearray, order: Literal[enums.ENDIAN.BIG, enums.ENDI
 class ARK():
     KEY = [0x3d5b2a34, 0x923fff10, 0x00e346a4, 0x0c74902b]
     
-    HEADER_MAP = {
-        "file_count": 4,
-        "metadata_offset": 4,
-        "ark_version": 4,
-    }
-    
-    header: types.Header
-    metadata: list[types.FileMetadata]
+    header: Header
+    metadata: list[FileMetadata]
     _decompresser = zstandard.ZstdDecompressor()
     
     def __init__(
@@ -57,11 +69,7 @@ class ARK():
             output (str | None, optional): Optional output folder to extract files to. Defaults to None.
         """
         self.files: list[ARKFile] = []
-        self.header = {
-            "file_count": 0,
-            "metadata_offset": 0,
-            "ark_version": 0,
-        }
+        self.header = Header()
         
         if file != None:
             self.read(file, output)
@@ -95,10 +103,9 @@ class ARK():
         with context_manager as open_file:
             open_file.seek(0)
             
-            self.header = self.read_header(open_file)
-            assert self.header['ark_version'] == 3
+            self.header = self._read_header(open_file)
 
-            self.metadata = self.get_metadata(open_file)
+            self.metadata = self._get_metadata(open_file)
             # print(self.metadata)
             
             for file_metadata in track(
@@ -106,18 +113,36 @@ class ARK():
                 console = console,
                 description = 'Extracting...',
             ):
-                filename = posix_path(os.path.join(file_metadata['pathname'], file_metadata['filename']))
+                filename = posix_path(os.path.join(decode(file_metadata.pathname), decode(file_metadata.filename)))
                 console.print(f'extracting [yellow]{filename}[/yellow]')
-                ark_file = ARKFile(
-                    os.path.join(file_metadata['pathname'], file_metadata['filename']),
-                    self.get_file_data(file_metadata, open_file),
-                )
+                ark_file = self._get_file_data(file_metadata, open_file)
                 self.files.append(ark_file)
                 
                 if output:
                     ark_file.save(os.path.join(output, ark_file.fullpath))
     
-    def read_header(self, file: IO) -> types.Header:
+    def write(self, file: str | bytes | bytearray | BinaryIO):
+        if isinstance(file, str):
+            context_manager = open(file, 'wb')
+        elif is_binary_file(file):
+            context_manager = contextlib.nullcontext(file)
+        elif is_text_file(file):
+            raise TypeError('file must be open in binary mode')
+        else:
+            raise TypeError('cannot open file')
+        
+        with context_manager as open_file:
+            open_file.seek(0)
+            packed_files = self._pack_files()
+            self.header.metadata_offset = dcs.get_struct_size(Header)
+            for data, meta in packed_files:
+                meta.file_location = self.header.metadata_offset
+                self.header.metadata_offset += len(data)
+            
+            self._write_header(open_file)
+            self._write_files_and_metadata(open_file, packed_files)
+    
+    def _read_header(self, file: IO) -> Header:
         """Read the header of a `.ark` file.
 
         Args:
@@ -126,14 +151,18 @@ class ARK():
         Returns:
             dict: Header.
         """
-        header = {}
-        
-        for key, length in self.HEADER_MAP.items():
-            header[key] = int.from_bytes(file.read(length), 'little')
+        header = Header.from_packed(
+            file.read(dcs.get_struct_size(Header))
+        )
         
         return header
+    
+    def _write_header(self, file: IO):
+        self.header.file_count = len(self.files)
+        self.header.metadata_offset
+        file.write(self.header.pack())
 
-    def get_metadata(self, file: IO) -> None | list[types.FileMetadata]:
+    def _get_metadata(self, file: IO) -> None | list[FileMetadata]:
         filesize: int = None
         
         file.seek(0, os.SEEK_END)
@@ -144,77 +173,104 @@ class ARK():
         if filesize < 0:
             raise TypeError('file size is negative, somehow...')
         
-        metadata_size = tea.get_phdr_size(filesize - self.header['metadata_offset'])
+        metadata_size = xxtea.get_phdr_size(filesize - self.header.metadata_offset)
         # print(f'metadata size: {metadata_size}')
-        raw_metadata_size = self.header["file_count"] * sum(FILE_METADATA_MAP.values())
+        raw_metadata_size = self.header.file_count * dcs.get_struct_size(Header)
         # print(f'raw metadata size: {raw_metadata_size}')
         
-        file.seek(self.header["metadata_offset"], os.SEEK_SET)
+        file.seek(self.header.metadata_offset, os.SEEK_SET)
         
         metadata = file.read(metadata_size)
         
         # print(f'metadata: {int.from_bytes(metadata, 'little')}')
-        metadata = tea.decrypt(metadata, metadata_size // 4, self.KEY)
+        metadata = xxtea.decrypt(metadata, metadata_size // 4, self.KEY)
         
         raw_metadata = self._decompresser.decompress(metadata, raw_metadata_size)
 
-        header_size = sum(FILE_METADATA_MAP.values())
+        metadata_size = dcs.get_struct_size(FileMetadata)
         result = []
-        for file_index in range(self.header["file_count"]):
-            file_result = {}
+        for file_index in range(self.header.file_count):
+            offset = file_index * metadata_size
             
-            offset = file_index * header_size
-            
-            pos = 0
-            
-            for key, length in FILE_METADATA_MAP.items():
-                file_result[key] = raw_metadata[offset + pos:offset + pos + length]
-
-                pos += length
+            file_result = FileMetadata.from_packed(
+                raw_metadata[offset : offset + metadata_size]
+            )
             
             result.append(file_result)
 
-        return self.fix_metadata(result)
+        return result
     
-    def fix_metadata(self, metadata: list[types.RawFileMetadata]):
-        fixed_metadata: list[types.FileMetadata] = []
-
+    def _fix_metadata(self, metadata: list[FileMetadata]):
         for file in metadata:
-            fixed_file: types.FileMetadata = {}
 
-            fixed_file['filename'] = file['filename'].decode('ascii').rstrip('\x00')
-            fixed_file['pathname'] = file['pathname'].decode('ascii').rstrip('\x00')
-            fixed_file["file_location"] = int.from_bytes(file["file_location"], 'little')
-            fixed_file['original_filesize'] = int.from_bytes(file['original_filesize'], 'little')
-            fixed_file['compressed_size'] = int.from_bytes(file['compressed_size'], 'little')
-            fixed_file['encrypted_nbytes'] = int.from_bytes(file['encrypted_nbytes'], 'little')
-            fixed_file['timestamp'] = int.from_bytes(file['timestamp'], 'little')
-            fixed_file['md5sum'] = file['md5sum'].hex()
-            fixed_file['priority'] = int.from_bytes(file['priority'], 'little')
-
-            fixed_metadata.append(fixed_file)
+            file.filename = file.filename.decode('ascii').rstrip('\x00')
+            file.pathname = file.pathname.decode('ascii').rstrip('\x00')
+            file.md5sum = file.md5sum.hex()
         
-        return fixed_metadata
+        return metadata
             
 
-    def get_file_data(self, metadata: types.FileMetadata, file: IO):
-        file.seek(metadata['file_location'], os.SEEK_SET)
+    def _get_file_data(self, metadata: FileMetadata, file: IO):
+        file.seek(metadata.file_location, os.SEEK_SET)
         
-        file_data = file.read(metadata['encrypted_nbytes'] if metadata['encrypted_nbytes'] else metadata['compressed_size'])
+        file_data = file.read(metadata.encrypted_nbytes if metadata.encrypted_nbytes else metadata.compressed_size)
 
-        if (metadata['encrypted_nbytes']) != 0:
-            file_data = tea.decrypt(file_data, metadata['encrypted_nbytes'] // 4, self.KEY)
+        compressed = False
+        encrypted = False
+
+        if (metadata.encrypted_nbytes) != 0:
+            encrypted = True
+            file_data = xxtea.decrypt(file_data, metadata.encrypted_nbytes // 4, self.KEY)
         
-        if (metadata['compressed_size'] != metadata['original_filesize']):
-            file_data = self._decompresser.decompress(file_data, metadata['original_filesize'])
+        if (metadata.compressed_size != metadata.original_filesize):
+            compressed = True
+            file_data = self._decompresser.decompress(file_data, metadata.original_filesize)
         
-        if hashlib.md5(file_data).hexdigest() != metadata['md5sum']:
-            warnings.warn(f'file "{posix_path(os.path.join(metadata['pathname'], metadata['filename']))}" hash does not match "{metadata['md5sum']}"')
+        if hashlib.md5(file_data).hexdigest() != metadata.md5sum.hex():
+            warnings.warn(f'file "{posix_path(os.path.join(decode(metadata.pathname), decode(metadata.filename)))}" hash does not match "{metadata.md5sum.hex()}"')
         
-        return file_data
+        return ARKFile(
+            os.path.join(decode(metadata.pathname), decode(metadata.filename)),
+            file_data,
+            encrypted = encrypted,
+            compressed = compressed,
+            priority = metadata.priority,
+        )
+
+    def _pack_files(self) -> list[tuple[bytes, FileMetadata]]:
+        packed = []
+        
+        for file in self.files:
+            if not isinstance(file, ARKFile):
+                raise TypeError('file must be instance of ARKFile')
+            
+            data, meta = file.pack()
+            
+            packed.append((data, meta))
+        
+        return packed
+
+    def _write_files_and_metadata(self, file: IO, packed_files: list[tuple[bytes, FileMetadata]]):
+        metadata_block: bytes = b''
+        for data, meta in packed_files:
+            file.seek(meta.file_location)
+            file.write(data)
+            metadata_block += meta.pack()
+        
+        file.seek(self.header.metadata_offset)
+        metadata_block = zstandard.compress(metadata_block)
+        metadata_block = xxtea.encrypt(metadata_block, len(metadata_block) // 4, self.KEY)
+        file.write(metadata_block)
         
 class ARKFile():
-    def __init__(self, filename: str, data: bytes) -> None:
+    def __init__(
+        self,
+        filename: str,
+        data: bytes,
+        compressed: bool = True,
+        encrypted: bool = False,
+        priority: int = 0,
+    ) -> None:
         """File inside `.ark` file.
 
         Args:
@@ -223,6 +279,11 @@ class ARKFile():
         """
         self.fullpath = str(filename)
         self.data: bytes = bytes(data)
+        
+        self.compressed = compressed
+        self.encrypted = encrypted
+        
+        self.priority = priority
     
     @property
     def filename(self) -> str:
@@ -275,3 +336,26 @@ class ARKFile():
         
         with open(path, 'wb') as file:
             file.write(self.data)
+    
+    def pack(self) -> bytes | FileMetadata:
+        result = self.data
+        metadata = FileMetadata(
+            self.filename.encode('ascii'),
+            self.pathname.encode('ascii'),
+            0,
+            len(result),
+            len(result),
+            0,
+            0,
+            bytes.fromhex(hashlib.md5(result).hexdigest()),
+            self.priority,
+        )
+        if self.compressed:
+            result = zstandard.compress(result, 9)
+            metadata.compressed_size = len(result)
+
+        if self.encrypted:
+            result = xxtea.encrypt(result, len(result) // 4, ARK.KEY)
+            metadata.encrypted_nbytes = len(result)
+        
+        return result, metadata
