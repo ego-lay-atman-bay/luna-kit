9ec0c7fa2fa1c86a8c69d220b75d36667df79eaf
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-12-04 15:59:02 -0700

    remove all imports
    This so that it doesn't load unneeded classes if you're using luna kit for one specific thing.

--- luna_kit/__init__.py
+++ luna_kit/__init__.py
@@ -1,8 +1,2 @@
 __version__ = "1.0.0"
 __author__ = "ego-lay-atman-bay"
-
-from . import enums
-from .ark import ARK, ARKFile
-from .loc import LocalizationFile
-from .texatlas import TexAtlas
-from .rk import RKFormat
