c8b751b92f9e59093006d2f0b27786ef1c3dd390
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-12-13 19:46:36 -0700

    Create pvr.py

---
+++ luna_kit/cli/pvr.py
@@ -0,0 +1,125 @@
+from .cli import CLI, CLICommand
+from ._actions import GlobFiles
+
+from ..console import console
+@CLI.register_command
+class PVRCommand(CLICommand):
+    COMMAND = "pvr"
+    HELP = "Read PVR files encoded with astc encoding."
+    
+    @classmethod
+    def build_args(cls, parser):
+        parser.add_argument(
+            'files',
+            action = GlobFiles,
+            help = 'File or files to read',
+        )
+        
+        parser.add_argument(
+            '-s', '--show',
+            dest = 'show',
+            action = 'store_true',
+            help = 'Show the pvr file in the default image viewer.',
+        )
+        
+        parser.add_argument(
+            '-o', '--output',
+            dest = 'output',
+            help = 'Output file. Can be directory or filename including {name} and {ext}',
+        )
+        
+        parser.add_argument(
+            '-f', '--format',
+            dest = 'format',
+            help = 'Output format to save to. If this is omitted, it will use the format guessed from the output file extension. If the output is a folder, then it will use png.',
+        )
+        
+        confirm = parser.add_mutually_exclusive_group()
+        
+        confirm.add_argument(
+            '-y', '--yes',
+            dest = 'yes',
+            action = 'store_true',
+            help = 'Override files without confirmation.',
+        )
+        
+        confirm.add_argument(
+            '-n', '--no',
+            dest = 'no',
+            action = 'store_true',
+            help = "Don't override existing files without confirmation.",
+        )
+    
+    @classmethod
+    def run_command(cls, args):
+        import os
+        from ..pvr import PVR
+        from ..utils import get_PIL_format, strToBool
+        from ..safe_format import safe_format
+        
+        def save_image(file: str, ):
+            pvr = PVR(file)
+            output = args.output
+            name = os.path.splitext(os.path.basename(file))[0]
+            if output:
+                output = safe_format(
+                    output,
+                    name = name,
+                )
+                if output == args.output:
+                    if len(args.files) > 1:
+                        output = os.path.join(output, name + '.{format}')
+                        if args.format is None:
+                            args.format = "PNG"
+                
+                ext = os.path.splitext(os.path.basename(output))[1]
+                if args.format is None:
+                    format = ext[1:]
+                    
+                    try:
+                        format = get_PIL_format(ext)
+                    except ValueError:
+                        output = safe_format(
+                            output,
+                            format = format.lower(),
+                        )
+                        ext = os.path.splitext(os.path.basename(output))[1]
+                        
+                        try:
+                            format = get_PIL_format(ext)
+                        except ValueError:
+                            format = ext[1:]
+                else:
+                    format = args.format
+                
+                output = safe_format(
+                    output,
+                    format = format.lower(),
+                )
+                
+                dir = os.path.dirname(output)
+                if dir:
+                    os.makedirs(os.path.dirname(output), exist_ok = True)
+                override = True
+                if os.path.exists(output):
+                    override = None
+                    if args.yes or args.no:
+                        override = args.yes
+                    if override is None:
+                        
+                        override = console.input(f'[yellow]{output}[/yellow] already exists, do you want to override it? [dim](Y/n)[/]: ')
+                        if override == '':
+                            override = True
+                        else:
+                            override = strToBool(override)
+                
+                if override:
+                    pvr.save(output)
+            if args.show:
+                pvr.show(name)
+                
+            
+        
+        for file in args.files:
+            save_image(file)
+        
