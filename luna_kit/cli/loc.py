37c027efce46a479a0dc91ef8a3f20437adf6b2c
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2024-12-24 13:55:16 -0700

    make loc reading better
    You can now use a glob pattern to convert many files at once. I also added the ability to change the format from json to csv.

--- luna_kit/cli/loc.py
+++ luna_kit/cli/loc.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, Namespace
 from .cli import CLI, CLICommand
+from ._actions import GlobFiles
 
 @CLI.register_command
 class LOCCommand(CLICommand):
@@ -9,14 +10,16 @@ class LOCCommand(CLICommand):
     @classmethod
     def build_args(cls, parser: ArgumentParser):
         parser.add_argument(
-            'file',
-            help = 'input .loc file',
+            'files',
+            nargs = '+',
+            action = GlobFiles,
+            help = 'input .loc file(s)',
         )
         
         parser.add_argument(
             '-o', '--output',
             dest = 'output',
-            help = 'output filename, such as "english.json"',
+            help = 'output filename, such as "english.json". Can also have {name} to replace the filename.',
         )
         
         parser.add_argument(
@@ -25,25 +28,48 @@ class LOCCommand(CLICommand):
             action = 'store_true',
             help = 'Override output file if it already exists without prompt.',
         )
+        
+        parser.add_argument(
+            '-f', '--format',
+            dest = 'format',
+            choices = ['json', 'csv'],
+            help = "Output format.",
+            default = 'json',
+        )
+        
+        parser.add_argument(
+            '-i', '--indent',
+            dest = 'indent',
+            type = int,
+            help = 'Json indent.'
+        )
+        
     
     @classmethod
     def run_command(cls, args: Namespace):
         from ..loc import LOC
         import os
         from ..console import console
+        import csv
+        
+        for file in args.files:
+            output: str = f'{{name}}.{args.format}'
+            if args.output:
+                output = args.output
             
-        if not os.path.isfile(args.file):
-            raise FileNotFoundError(f'file "{args.file}" does not exist or is a directory.')
-        
-        output = os.path.splitext(os.path.basename(args.file))[0] + '.json'
-        if args.output:
-            output = output
-        
-        if not args.override and os.path.exists(output):
-            if input(f'"{output}" already exists, do you want to override it? (Y/n): ').lower() in ['n', 'no', 'false', '0']:
-                exit()
-        
-        loc_file = LOC(args.file)
-        loc_file.export(output, indent = 2)
-        
-        console.print(f'saved to {output}')
+            output = output.format(name = os.path.splitext(os.path.basename(file))[0])
+            
+            if not args.override and os.path.exists(output):
+                if input(f'"{output}" already exists, do you want to override it? (Y/n): ').lower() in ['n', 'no', 'false', '0']:
+                    continue
+            
+            loc_file = LOC(file)
+            if args.format == 'json':
+                loc_file.export(output, indent = args.indent)
+            elif args.format == 'csv':
+                with open(output, 'w', newline = '', encoding = 'utf-8') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(['key', 'string'])
+                    writer.writerows(loc_file.strings.items())
+            
+            console.print(f'saved [yellow]{output}[/]')
