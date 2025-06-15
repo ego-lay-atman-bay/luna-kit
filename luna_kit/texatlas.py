2f93f08bfcb01af5a51843ed23c92fec5b3b5b54
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2025-06-15 01:18:35 -0600

    search for pvr image if png couldn't be found

--- luna_kit/texatlas.py
+++ luna_kit/texatlas.py
@@ -6,14 +6,15 @@ import pathlib
 
 from PIL import Image
 
-from .file_utils import is_binary_file, is_text_file
+from .file_utils import is_binary_file, is_text_file, PathOrFile
 from .utils import posix_path, strToInt
+from .pvr import PVR
 
 
 class TexAtlas():
     def __init__(
         self,
-        file: str,
+        file: PathOrFile,
         search_folders: list[str] | None = None,
         smart_search: bool = True,
     ) -> None:
@@ -38,10 +39,11 @@ class TexAtlas():
                     'width': strToInt(line.get('width', 0)),
                     'height': strToInt(line.get('height', 0)),
                 } for line in csv.DictReader(
-                csvfile,
-                ['filename', 'atlas', 'x', 'y', 'width', 'height'],
-                delimiter = '\t',
-            ))
+                    csvfile,
+                    ['filename', 'atlas', 'x', 'y', 'width', 'height'],
+                    delimiter = '\t',
+                )
+            )
         
         if search_folders == None:
             search_folders = ['.']
@@ -65,7 +67,10 @@ class TexAtlas():
                 new_atlas_file,
             )):
                 atlas_file = new_atlas_file
-                atlas_image = Image.open(atlas_file)
+                if atlas_file.endswith('.pvr'):
+                    atlas_image = PVR(atlas_file).image
+                else:
+                    atlas_image = Image.open(atlas_file)
             
             self.images.append(
                 Texture(
@@ -77,23 +82,44 @@ class TexAtlas():
                         image_data['x'] + image_data['width'] - 1,
                         image_data['y'] + image_data['height'] - 1,
                     )),
-                    dir = posix_path(atlas_file).removesuffix('/' + posix_path(image_data['atlas']))
+                    dir = posix_path(os.path.dirname(atlas_file))
                 )
             )
             
-                
+    def get_image(self, image_data: dict[str, str | int]):
+        atlas_file = self.find_file(image_data['atlas'])
+        
+        atlas_image = Image.open(atlas_file)
+        
+        return Texture(
+            image_data['filename'],
+            image_data['atlas'],
+            atlas_image.crop((
+                image_data['x'],
+                image_data['y'],
+                image_data['x'] + image_data['width'] - 1,
+                image_data['y'] + image_data['height'] - 1,
+            )),
+            dir = posix_path(atlas_file).removesuffix('/' + posix_path(image_data['atlas']))
+        )
     
     def find_file(self, path: str):
+        pvr_name = os.path.splitext(path)[0] + '.pvr'
         for dir in self.search_folders:
             if os.path.isfile(filename := os.path.join(dir, path)):
+                return filename
+            elif os.path.isfile(filename := os.path.join(dir, pvr_name)):
                 return filename
         
         if self.smart_search and self.filename:
             path_obj = pathlib.Path(path)
+            pvr_obj = pathlib.Path(pvr_name)
             filename_path = pathlib.Path(os.path.dirname(self.filename)).absolute()
             
             while len(filename_path.parts) > 1:
                 if os.path.exists(filename := os.path.join(filename_path, path_obj)):
+                    return filename
+                elif os.path.exists(filename := os.path.join(filename_path, pvr_obj)):
                     return filename
                 
                 filename_path = pathlib.Path(*filename_path.parts[:-1])
