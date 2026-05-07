bc40a9845bddcdd7dda8620f4e4992a9b5d4f021
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-05-06 20:05:58 -0600

    Add custom `track()` function

--- luna_kit/console.py
+++ luna_kit/console.py
@@ -1,2 +1,26 @@
 from rich.console import Console
+from rich.progress import Progress, ProgressType, ProgressColumn
+from typing import Union, Optional
+from collections.abc import Iterable, Sequence
+
 console = Console(quiet = False)
+
+
+def track(
+    sequence: Union[Iterable[ProgressType], Sequence[ProgressType]],
+    total: Optional[float] = None,
+    description: str = 'Working...',
+    transient: bool = False,
+    columns: list[str | ProgressColumn] | None = None,
+):
+    progress = Progress(
+        *(columns or ()),
+        console = console,
+        transient = transient,
+    )
+
+    with progress:
+        yield from progress.track(
+            sequence = sequence,
+            description = description,
+        )
