3027f7dc5d96a384364f5e7d77d7ec76bd78a0a1
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-05-06 20:07:37 -0600

    Update defaultGameCampaign.py

--- luna_kit/typings/defaultGameCampaign.py
+++ luna_kit/typings/defaultGameCampaign.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Literal, Optional, Required, Mapping
+from typing import TypedDict, Literal, Optional, NotRequired, Required, Mapping
 
 __all__ = ['DefaultGameCampaignType']
 
@@ -112,7 +112,7 @@ class PiggyBank(TypedDict):
 # mapzones
 
 class MapzoneSpecialUnlock(TypedDict):
-    IsEnabled: Optional[bool]
+    IsEnabled: NotRequired[bool]
     CurrencyCount: int
     CurrencyName: str
     AltCurrencyCount: int
@@ -126,7 +126,7 @@ class SpawnEnemyLimits(TypedDict):
 
 
 class Mapzone(TypedDict):
-    SpecialUnlock: Optional[MapzoneSpecialUnlock]
+    SpecialUnlock: NotRequired[MapzoneSpecialUnlock]
     expand_spawn_enemy_limits: SpawnEnemyLimits
     trash_spawn_enemy: SpawnEnemyLimits
 
@@ -324,7 +324,274 @@ class MiniGames(TypedDict):
     dailygoals: DailyGoals
     newcamer_calendar: NewcomerCalendar
     ponybookminigame: dict[Literal['swap_cost_gem'], int]
-    
+
+# group quests
+
+class GroupQuests_ChestLootItem(TypedDict):
+    item: str
+    max: int
+    weight: int
+    min: int
+
+class GroupQuests_SPReward(TypedDict):
+    item: str
+    bad_chance: float
+    max: int
+    good_chance: float
+    min: int
+
+class GroupQuests_ConsolationPrize(TypedDict):
+    item: str
+    max: int
+    min: int
+
+class GroupQuests_Chest(TypedDict):
+    Items: list[GroupQuests_ChestLootItem]
+    Amount: int
+    Requirement: int
+    Icon: str
+
+class GroupQuests_Slot(TypedDict):
+    sp_chance: float
+    sp_rewards: list[GroupQuests_SPReward]
+    difficulty: float
+    consolation_prize: GroupQuests_ConsolationPrize
+    enabled: bool
+    skip_gems_per_hour: int
+    multiplier_step: float
+    multiplier_requirements: list[int]
+    loot: list[GroupQuests_Chest]
+    sp_delay_minutes: int
+
+class GroupQuests_SeasonalSlot(GroupQuests_Slot):
+    seasonal: str
+    season_start_time: int
+
+class GroupQuests(TypedDict):
+    tokens_per_chest: int
+    iv_skip_minutes: list[int]
+    token_id_per_chest: Literal['Token_GQ_bonus']
+    tutorial_slot: GroupQuests_Slot
+    slots: list[GroupQuests_Slot]
+    random_pros: list[str]
+    seasonal_slots: list[GroupQuests_SeasonalSlot]
+    alt_prize: list[GroupQuests_ChestLootItem]
+
+# GlobalDefines
+
+class GlobalDefines_SocialTreasureDrop(TypedDict):
+    amount: int
+    id: str
+
+class GlobalDefines_Derpy(TypedDict):
+    gem_chance: float
+    minecart_wheel_chance: float
+    spawn_time: int
+    find_coin_reward: int
+    min_exist_time: int
+    find_time: int
+    respawn_timer: int
+
+class GlobalDefines_UpdateGift(TypedDict):
+    amount: int
+    id: str
+
+class GlobalDefines_VacuumCleaner(TypedDict):
+    is_enabled: bool
+    one_hour_gem_price: int
+    required_lvl: int
+    override: dict[Literal[
+            'canterlot',
+            'saa',
+            'ponyville',
+            'ce',
+            'klugetown',
+        ], dict[Literal[
+            'attract_shop_count',
+            'next_use_delay',
+        ], int]]
+    attract_shop_count: int
+    next_use_delay: int
+
+class GlobalDefines_MysteryPony(TypedDict):
+    gem_chance: float
+    spawn_time: int
+    find_coin_reward: int
+    min_exist_time: int
+    find_leaderboard_currency: int
+    find_time: int
+    respawn_timer: int
+
+class GlobalDefines_ParaspritesGenerator(TypedDict):
+    min_shards: dict[Literal[
+        'Laughter_min_shard',
+        'Honesty_min_shard',
+        'Kindness_min_shard',
+        'Magic_min_shard',
+        'Loyalty_min_shard',
+        'Generosity_min_shard',
+    ], int]
+    amx_shards: dict[Literal[
+        'Laughter_max_group',
+        'Honesty_max_group',
+        'Kindness_max_group',
+        'Magic_max_group',
+        'Loyalty_max_group',
+        'Generosity_max_group',
+    ], int]
+
+class GlobalDefines_EventConversion(TypedDict):
+    Gems: int
+    Currency: int
+
+class GlobalDefines_TokenDrop(TypedDict):
+    shop_chance: int
+    type: str
+
+class GlobalDefines_IAPSort(TypedDict):
+    promo_first: bool
+    order: str
+
+class GlobalDefines_SubPointcut(TypedDict):
+    pointcutId: str
+    limit: int
+
+class GlobalDefines_SubWelcomeGift(TypedDict):
+    Quantity: int
+    GiftId: str
+
+class GlobalDefines_SubscribeData(TypedDict):
+    available: bool
+    firstSubscribeAwardGems: int
+    WheelsSubscriptionCap: int
+    AltPointcuts: list[GlobalDefines_SubPointcut]
+    MCRevivesPerRide: int
+    vacuumTimer: int
+    subscriptionDuration: int
+    ShopBonus: int
+    PonyTaskBoost: float
+    SubscriptionPlusWelcomeGifts: list[GlobalDefines_SubWelcomeGift]
+
+
+class FortuneShopItemType(TypedDict):
+    id: str
+    amount: NotRequired[int]
+    price_list: list[int]
+    sub_price_list: NotRequired[list[int]]
+
+class FortuneShopDataType(TypedDict):
+    max_items_in_shop: int
+    refresh_cost: int
+    refresh_period_days: int
+    item_discount_chances: NotRequired[dict[Literal[
+        'discount',
+        'regular',
+        'super',
+    ], int]]
+    item_price_chances: NotRequired[dict[Literal[
+        'regular',
+        'discount',
+        'super',
+        'ultra',
+    ], int]]
+    item_rarity_chances: dict[Literal[
+        'common',
+        'uncommon',
+        'rare',
+    ], int]
+    required_level: int
+    item_lists: dict[Literal[
+        'common',
+        'uncommon',
+        'rare',
+    ], list[FortuneShopItemType]]
+
+
+class GlobalDefines(TypedDict):
+    edit_force_theme_level: int
+    energy_restore_cost: int
+    change_sectors_image_spinning_wheel_time: int
+    change_name_cost: int
+    forced_oft: str
+    increment_spinning_wheel_cost_time: int
+    mini_games: dict[Literal[
+        'pop_level_requirement',
+        'minecart_max_score',
+        'minecart_level_requirement',
+        'eg_level_requirement'
+    ], int]
+    social_treasure_drop: NotRequired[list[GlobalDefines_SocialTreasureDrop]]
+    rate_our_game_enabled: bool
+    zh_max_ingredients: int
+    player_max_level: int
+    tickets_cap: int
+    current_subseason: int
+    shop_minecart_wheel_drop_chance: float
+    derpy: GlobalDefines_Derpy
+    zecora_totem_ads: dict[Literal['skip_time'], int]
+    ads_tls_currency_amounts: dict[Literal['arena_amounts', 'blitz_amounts'], list[int]]
+    ads_currency_amount: dict[Literal['bits_amount', 'gems_amount'], int]
+    oft_exclusion_country_name: str
+    coppa_age: int
+    contacts: dict[str, str]
+    coppa_enabled_countries: str
+    update_gift: list[GlobalDefines_UpdateGift]
+    chest_minecart_wheel_drop_chance: float
+    oft_age: int
+    gdpr_enabled_countries: str
+    offline_shop: dict[Literal['off_string_secondary_id', 'off_string_primary_id'], str]
+    spinning_wheel_cooldown_adv_times: int
+    speedup_data: dict[Literal[
+            'subscribe',
+            'regular',
+            'subsribe_plus',
+        ], dict[Literal[
+            'fuelcapacity',
+            'restock',
+            'deltaspeed',
+        ], int]]
+    forced_coppa: str
+    ads_reward_pony: dict
+    mega_event_required_level: int
+    disclaimerCountries: str
+    coppa_exclusion_country_name: str
+    tickets_restore_cost: int
+    free_lotto_wait_time: int
+    vacuum_cleaner: GlobalDefines_VacuumCleaner
+    minimal_level_for_ponyversary: int
+    season: str
+    tq_mystery_pony: GlobalDefines_MysteryPony
+    ads_params: dict[Literal['iap_building_freecash'], int]
+    parasprites_generator: GlobalDefines_ParaspritesGenerator
+    decore_minecart_wheel_drop_chance: float
+    shards_spending_data: dict[Literal['spend_prices'], list[int]]
+    didomi: dict[str, str]
+    iv_farm_skip_min: list[int]
+    energy_restore_time: int
+    extra_promo_OFT_enabled: bool
+    blitz_default_convertation_rate: list[GlobalDefines_EventConversion]
+    token_drop: GlobalDefines_TokenDrop
+    mega_event_default_convertation_rate: list[GlobalDefines_EventConversion]
+    current_season: int
+    fortune_shop_data: FortuneShopDataType
+    personal_shop_data: FortuneShopDataType
+    oft_enabled_countries: str
+    iap_sort: GlobalDefines_IAPSort
+    news_enable_OFT_COPPA: int
+    atv_news_forum_enabled: int
+    free_spinning_wheel_wait_time: int
+    tickets_restore_time: int
+    iv_animal_skip_min: list[int]
+    quest_ads_data: dict[Literal[
+        'minLvLForXPMultiply',
+        'gems_limit',
+        'rewardMultiplier',
+        'bits_limit',
+    ], int]
+    tickets_restore_cost_new: None
+    SubscribeData: GlobalDefines_SubscribeData
+    energy_cap: int
+
 
 # DefaultGameCampaign
 class DefaultGameCampaignType(TypedDict):
@@ -344,14 +611,14 @@ class DefaultGameCampaignType(TypedDict):
     CK_Boss: CK_Boss
     mini_games: MiniGames
     enable_auto_refresh: int
-    global_defines: dict
+    global_defines: GlobalDefines
     CollectionsData: dict
     player: dict
     init: dict
     VIP: dict
     Redeem: dict
     item_fuse: dict
-    group_quests: dict
+    group_quests: GroupQuests
     ReferralRewards: dict
     game_object_data: dict
     events: dict
