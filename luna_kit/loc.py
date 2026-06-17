76b2225535865be7b10b3f94b8c2fba0d327262b
ego-lay_atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2026-06-17 01:39:21 -0600

    Make sure `translate()` is case insensitive
    I noticed that sometimes the game has wrong casing, so I'm just forcing the `translate()` function to find the key regardless of the case.

--- luna_kit/loc.py
+++ luna_kit/loc.py
@@ -122,8 +122,8 @@ class LOC(UserDict):
         file.write(encoded)
 
     def translate(self, key: str):
-        data = {key.strip(): value for key, value in self.data.items()}
-        return data.get(key.strip(), key)
+        data = {key.strip().upper(): value for key, value in self.data.items()}
+        return data.get(key.strip().upper(), key)
     
     @property
     def language(self):
